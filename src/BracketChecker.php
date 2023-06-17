7f219b4f5e8882ef8e9b921fe190f8d774e042ed
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-06-17 12:07:47 +0300

    Add src files

---
+++ src/BracketChecker.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Vladimirsannikov\Bracketchecker;
+
+require_once "Response.php";
+
+class BracketChecker
+{
+    public function check(): void
+    {
+        $brackets = $_REQUEST['string'] ?? null;
+
+        if ($brackets == null) {
+            throw new EmptyBracketSequenceException();
+        }
+
+        $bracketCounter = 0;
+
+        for ($i = 0; $i < strlen($brackets); $i++) {
+            if ($brackets[$i] == '(') {
+                $bracketCounter++;
+            } elseif ($brackets[$i] == ')') {
+                $bracketCounter--;
+                if ($bracketCounter < 0) {
+                    break;
+                }
+            }
+        }
+
+        if ($bracketCounter == 0) {
+            $content = 'Правильная скобочная последовательность';
+            $response = new Response($content);
+        } else {
+            $status = 400;
+            $content = 'Неправильная скобочная последовательность';
+            $response = new Response($content, $status);
+        }
+        $response->provideResponse();
+    }
+}
